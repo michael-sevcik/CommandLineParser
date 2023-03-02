028678c92f980b7f088228a4d4a4320f2dcc8ec4
Michael Ševčík <69696161@cuni.cz>
2023-03-02 17:34:54 +0100

    Refactor Parser's interface

--- CommandLineParser/ArgumentParsing/Parser.cs
+++ CommandLineParser/ArgumentParsing/Parser.cs
@@ -11,16 +11,20 @@ namespace ArgumentParsing
     /// </summary>
     public class Parser
     {
-        private List<string> _plainParamethers = new List<string>();
-        protected OptionSet options;
+        private List<string> _plainParamethers = new();
+        private OptionSet options = new();
 
         /// <summary>
-        /// Creates instance of Parser that parses given options.
+        /// Adds the option to the OptionSet.
         /// </summary>
-        /// <param name="optionSet">Set of options used for parsing.</param>
-        public Parser(OptionSet optionSet)
+        /// <param name="option"><see cref="IOption"> instance to be added.</param>
+        /// <returns>Returns true if there were no problems adding the option,
+        /// returns false if an error occurred, such as synonyms colliding with already added options, no short options and
+        /// no long options at the same time and other undefined behavior.
+        /// </returns>
+        public bool Add(IOption option)
         {
-            this.options = optionSet;
+            throw new NotImplementedException();
         }
 
         /// <summary>
@@ -63,51 +67,7 @@ namespace ArgumentParsing
 
     
 
-    /// <summary>
-    /// The OptionSet class is used to define the set of options that can be parsed by the Parser class.
-    /// </summary>
-    /// 
-    /// <Remarks>
-    /// If two or more options with the same identifier are added to the OptionSet, Find method could behave nondeterministically.
-    /// </Remarks>
-    public class OptionSet
-    {
-        private Dictionary<string, Option> longOptions = new();
-        private Dictionary<char, Option> shortOptions = new();
 
-        /// <summary>
-        /// Adds the option to the OptionSet.
-        /// </summary>
-        /// <param name="option"><see cref="Option"> instance to be added.</param>
-        /// <returns>Returns true if there were no problems adding an option to the OptionSet,
-        /// returns false if an error occured, such as synonyms colliding with already added options, no short options and
-        /// no long options at the same time and other undefined behaviour.
-        /// </returns>
-        public bool Add(Option option)
-        {
-            throw new NotImplementedException();
-        }
-
-        /// <summary>
-        /// Looks for option that was added to the OptionSet and has the specified identifier.
-        /// </summary>
-        /// <param name="shortIdentifier">Short identifier of an option.</param>
-        /// <returns>Returns an option if corresponding one was found, otherwise null.</returns>
-        public Option? Find(char shortIdentifier)
-        {
-            throw new NotImplementedException();
-        }
-
-        /// <summary>
-        /// Looks for option that was added to the OptionSet and has the specified identifier.
-        /// </summary>
-        /// <param name="longIdentifier">Long identifier of an option.</param>
-        /// <returns>Returns an option if corresponding one was found, otherwise null.</returns>
-        public Option? Find(string longIdentifier)
-        {
-            throw new NotImplementedException();
-        }
-    }
 
 
 
