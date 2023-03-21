2cb239df42fb97cb28ee890fc9440bd2512f7417
Lubomír Bulej <bulej@d3s.mff.cuni.cz>
2023-03-21 18:05:50 +0100

    Import review from s43

--- .nprg043/reviews/API_REVIEW-s43.md
+++ .nprg043/reviews/API_REVIEW-s43.md
@@ -1,1 +1,25 @@
-The reviewer did not (yet?) submit anything!
+# First impression #
+
+Alright, so I cloned the project, and I gotta say, it's looking pretty good so far. The README.md is easy to follow and gives me a nice overview of what the library does, which is helping me parse command-line arguments like a pro. It talks about use cases, has a little tutorial with examples, explains the main concepts, and even shows how I can extend the library if I want. Plus, there are instructions on how to build the library and the docs.
+
+# API review #
+
+The API is well thought out and user-friendly. It keeps things organized and has a clean interface for defining options and their properties. The library takes care of parsing and validating command-line input, and I just need to give it the right callbacks to handle the options.
+
+Working with the API is pretty comfy in common situations like setting up the parser, parsing the command line, getting the parsed values, and handling errors. The API docs are solid and give me a good idea of what each method does and what it expects from me. Overall, the library has everything I need for task-1.
+
+# Writing numactl #
+
+Using this library to implement the argument-processing part of numactl was a breeze. The concepts and methods provided by the library matched up nicely with what I needed for numactl. Defining options, callbacks, and parsing logic was easy peasy. I didn't run into any major hiccups or limitations.
+
+# Detailed comments #
+
+The library API lets me define parsable elements and their properties without breaking a sweat. It supports bounded integers, enums, and string parameter types, which is pretty versatile. It can also generate help text based on the docs I provide and handle custom parameter types.
+
+Error handling is on point, with clear error messages when something goes wrong. I can store the parsed values in my own callbacks and decide how to use them. The API is declarative and stateless, using immutable objects, and I can create multiple independent parsers without any global state messing things up.
+
+The library's external docs cover its goals and non-goals, key concepts, and design decisions. The source code docs are thorough, with public classes and methods well-documented. The example program is also well-documented, which helped a lot when writing the numactl implementation. The code follows platform conventions and has consistent formatting.
+
+# Conclusion #
+
+So, to wrap it up, this library is super helpful for parsing command-line arguments and makes it easy to implement something like numactl. Also looking at this implementation of API made me realize that my implementation could've been much better.
---
+++ NewExample/Program.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using ArgumentParsing;
+
+namespace NewExample {
+	class Program {
+		public static void Main(string[] args) {
+			var interleaveOption = IParametrizedOption.CreateParameterOption<string>(InterleaveAction, false, true, new char[] { 'i' }, new string[] { "interleave" });
+			interleaveOption.SetHelpString("Interleave memory allocation across given nodes.");
+
+			var preferredOption = IParametrizedOption.CreateParameterOption<string>(PreferredAction, false, true, new char[] { 'p' }, new string[] { "preferred" });
+			preferredOption.SetHelpString("Prefer memory allocations from given node.");
+
+			var membindOption = IParametrizedOption.CreateParameterOption<string>(MembindAction, false, true, new char[] { 'm' }, new string[] { "membind" });
+			membindOption.SetHelpString("Allocate memory from given nodes only.");
+
+			var physcpubindOption = IParametrizedOption.CreateParameterOption<string>(PhyscpubindAction, false, true, new char[] { 'C' }, new string[] { "physcpubind" });
+			physcpubindOption.SetHelpString("Run on given CPUs only.");
+
+			var showOption = IOption.CreateNoParameterOption(ShowAction, false, new char[] { 's' }, new string[] { "show" });
+			showOption.SetHelpString("Show current NUMA policy.");
+
+			var hardwareOption = IOption.CreateNoParameterOption(HardwareAction, false, new char[] { 'H' }, new string[] { "hardware" });
+			hardwareOption.SetHelpString("Print hardware configuration.");
+
+			Parser parser = new();
+			parser.SetPlainArgumentHelpString("Terminate option list.");
+
+			parser.Add(interleaveOption);
+			parser.Add(preferredOption);
+			parser.Add(membindOption);
+			parser.Add(physcpubindOption);
+			parser.Add(showOption);
+			parser.Add(hardwareOption);
+
+			if (args.Length == 0)
+			{
+				DisplayHelp();
+				return;
+			}
+
+			parser.ParseCommandLine(args);
+		}
+
+		private static void InterleaveAction(string nodes) {
+			Console.WriteLine($"Interleave memory allocation across nodes: {nodes}");
+		}
+
+		private static void PreferredAction(string node) {
+			Console.WriteLine($"Prefer memory allocations from node: {node}");
+		}
+
+		private static void MembindAction(string nodes) {
+			Console.WriteLine($"Allocate memory from nodes only: {nodes}");
+		}
+
+		private static void PhyscpubindAction(string cpus) {
+			Console.WriteLine($"Run on given CPUs only: {cpus}");
+		}
+
+		private static void ShowAction() {
+			Console.WriteLine("Show current NUMA policy.");
+			EmulateShowNumaPolicy();
+		}
+
+		private static void HardwareAction() {
+			Console.WriteLine("Print hardware configuration.");
+			EmulateHardwareInfo();
+		}
+
+		static void DisplayHelp() {
+			Console.WriteLine("Usage: numactl [--interleave= | -i <nodes>] [--preferred= | -p <node>]");
+			Console.WriteLine("               [--physcpubind= | -C <cpus>] [--membind= | -m <nodes>]");
+			Console.WriteLine("               command args ...");
+			Console.WriteLine("       numactl [--show | -S]");
+			Console.WriteLine("       numactl [--hardware | -H]");
+			Console.WriteLine();
+			Console.WriteLine("<nodes> is a comma-delimited list of node numbers or A-B ranges or all.");
+			Console.WriteLine("<cpus> is a comma-delimited list of cpu numbers or A-B ranges or all.");
+			Console.WriteLine();
+			Console.WriteLine("--interleave, -i   Interleave memory allocation across given nodes.");
+			Console.WriteLine("--preferred, -p    Prefer memory allocations from given node.");
+			Console.WriteLine("--membind, -m      Allocate memory from given nodes only.");
+			Console.WriteLine("--physcpubind, -C  Run on given CPUs only.");
+			Console.WriteLine("--show, -S         Show current NUMA policy.");
+			Console.WriteLine("--hardware, -H     Print hardware configuration.");
+		}
+
+
+		static void EmulateHardwareInfo() {
+			Console.WriteLine("available: 2 nodes (0-1)");
+			Console.WriteLine("node 0 cpus: 0 2 4 6 8 10 12 14 16 18 20 22");
+			Console.WriteLine("node 0 size: 24189 MB");
+			Console.WriteLine("node 0 free: 18796 MB");
+			Console.WriteLine("node 1 cpus: 1 3 5 7 9 11 13 15 17 19 21 23");
+			Console.WriteLine("node 1 size: 24088 MB");
+			Console.WriteLine("node 1 free: 16810 MB");
+			Console.WriteLine("node distances:");
+			Console.WriteLine("node   0   1");
+			Console.WriteLine("  0:  10  20");
+			Console.WriteLine("  1:  20  10");
+		}
+
+		static void EmulateShowNumaPolicy() {
+			Console.WriteLine("policy: default");
+			Console.WriteLine("preferred node: current");
+			Console.WriteLine("physcpubind: 0 1 2 3 4 5 6 7 8");
+			Console.WriteLine("cpubind: 0 1");
+			Console.WriteLine("nodebind: 0 1");
+			Console.WriteLine("membind: 0 1");
+		}
+	}
+}
