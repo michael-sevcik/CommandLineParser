58af1f5644ebbb0205a7275a819425329aef2536
Michael Ševčík <69696161@cuni.cz>
2023-03-26 16:01:18 +0200

    Update example program to use OptionBuilder

--- CommandParserExampleProgram/Program.cs
+++ CommandParserExampleProgram/Program.cs
@@ -11,48 +11,68 @@ namespace ExampleProgram
     {
         public static void Main(string[] args)
         {
-
-            // Create options.
-            Action<Format?> formatAction = format => Console.WriteLine(format);
-            var FormatOption = IParametrizedOption.CreateParameterOption<Format?>(formatAction, false, true, new char[] { 'f' }, new string[] { "format" });
-            FormatOption.SetHelpString("Specify output format, possibly overriding the format specified in the environment variable TIME.");
-
-            Action markPortable = () => Console.WriteLine("was portable");
-            var portabilityOption = IOption.CreateNoParameterOption(markPortable, false, new char[] { 'p' }, new string[] { "portability" });
-            portabilityOption.SetHelpString("Use the portable output format.");
-
-            Action<string?> processOutputFile = (string? OutputFileName) => Console.WriteLine();
-            var outputFileOption = IParametrizedOption.CreateParameterOption(processOutputFile, false, true, new char[] { 'o' }, new string[] { "output" });
-            outputFileOption.SetHelpString("Do not send the results to stderr, but overwrite the specified file.");
-
-            Action markAppend = () => Console.WriteLine("was append");
-            var appendOption = IOption.CreateNoParameterOption(markAppend, false, new char[] { 'a' }, new string[] { "append" });
-            appendOption.SetHelpString("(Used together with -o.) Do not overwrite but append.");
-
-
-            Action verboseAction = () => Console.WriteLine("Verbose output");
-            var verboseOutputOption = IOption.CreateNoParameterOption(verboseAction, false, new char[] { 'V' }, new string[] { "verbose" });
-            verboseOutputOption.SetHelpString("Give very verbose output about all the program knows about.");
-
-            Action markHelp = () => Console.WriteLine("was Help");
-            var helpOption = IOption.CreateNoParameterOption(markAppend, false, null, new string[] { "help" });
-            helpOption.SetHelpString("(Used together with -o.) Do not overwrite but append.");
-
-            
             // Create Parser.
             Parser parser = new();
             parser.SetPlainArgumentHelpString("Terminate option list.");
 
-            // Fill parser with already created options.
-            parser.Add(FormatOption);
-            parser.Add(portabilityOption);
-            parser.Add(outputFileOption);
-            parser.Add(appendOption);
-            parser.Add(verboseOutputOption);
-            parser.Add(helpOption);
+            var optionBuilder = new OptionBuilder();
+
+            // Create options via cool builder.
+
+            optionBuilder.Reset()
+                .WithShortSynonyms('f')
+                .WithLongSynonyms("format")
+                .WithParametrizedAction<Format?>(format => Console.WriteLine(format))
+                .RequiresParameter()
+                .WithHelpString("Specify output format, possibly overriding the format specified in the environment variable TIME.")
+                .RegisterOption(parser);             
+
+            optionBuilder.Reset()
+                .WithShortSynonyms('p')
+                .WithLongSynonyms("portability")
+                .WithAction(() => Console.WriteLine("was portable"))
+                .WithHelpString("Use the portable output format.")
+                .RegisterOption(parser);
+
+
+            optionBuilder.Reset()
+                .WithShortSynonyms('o')
+                .WithLongSynonyms("output")
+                .WithParametrizedAction<string?>((string? OutputFileName) => Console.WriteLine())
+                .RequiresParameter()
+                .WithHelpString("Do not send the results to stderr, but overwrite the specified file.")
+                .RegisterOption(parser);
+
+            Action markAppend = () => Console.WriteLine("was append");
+            optionBuilder.Reset()
+                .WithShortSynonyms('a')
+                .WithLongSynonyms("append")
+                .WithAction(() => Console.WriteLine("was append"))
+                .WithHelpString("(Used together with -o.) Do not overwrite but append.")
+                .RegisterOption(parser);
+
+            Action verboseAction = () => Console.WriteLine("Verbose output");
+            optionBuilder.Reset()
+                .WithShortSynonyms('V')
+                .WithLongSynonyms("verbose")
+                .WithAction(verboseAction)
+                .WithHelpString("Give very verbose output about all the program knows about.")
+                .RegisterOption(parser);
+
+            Action markHelp = () => Console.WriteLine("was Helped");
+            optionBuilder.Reset()
+                .WithLongSynonyms("help")
+                .WithAction(markHelp)
+                .WithHelpString("(Used together with -o.) Do not overwrite but append.")
+                .RegisterOption(parser);
 
             // Parse command-line input.
             parser.ParseCommandLine(args);
+
+            if (!parser.ParseCommandLine(args))
+            {
+                Console.WriteLine(parser.Error?.message);
+            }
         }
     }
 }
