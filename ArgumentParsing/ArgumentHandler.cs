41aa1410afd76bb62ffb2c9e75f079383e9bcf04
Michael Ševčík <69696161@cuni.cz>
2023-05-01 01:08:25 +0200

    Add ArgumentHandler

---
+++ ArgumentParsing/ArgumentHandler.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArgumentParsing;
+
+public partial class Parser
+{
+    partial class ArgumentProcessor
+    {
+        public struct ArgumentProcessingResult
+        {
+            public readonly IOption[] optionsToTakeAction;
+            public readonly IPlainArgument[] plainArgumentsToTakeAction;
+            public readonly string[] excessivePlainArgumentsEntries;
+
+            public ArgumentProcessingResult(
+                IOption[] optionsToTakeAction,
+                IPlainArgument[] plainArgumentsToTakeAction,
+                string[] excessivePlainArgumentsEntries)
+            {
+                this.optionsToTakeAction = optionsToTakeAction;
+                this.plainArgumentsToTakeAction = plainArgumentsToTakeAction;
+                this.excessivePlainArgumentsEntries = excessivePlainArgumentsEntries;
+            }
+        }
+
+        enum ParsingState : byte
+        {
+            PlainArguments,
+            Mixed
+        }
+
+        readonly IPlainArgument[]? plainArguments;
+        readonly OptionSet.OptionSet options;
+
+        HashSet<IOption> optionsToTakeAction = new();
+        HashSet<IPlainArgument> plainArgumentsToTakeAction = new();
+        List<string> excessivePlainArgumentsEntries = new();
+
+        IParametrizedOption? optionAwaitingParameter = null;
+
+        ParsingState state = ParsingState.Mixed;
+
+        int nextPlainArgumentPosition = 0;
+
+        /// <summary>
+        /// If an error occurs during parsing it gets a instance of <see cref="ParserError"/> that is describing the problem, otherwise null.
+        /// </summary>
+        public ParserError? Error { get; private set; } = null;
+
+        public ArgumentProcessor(Parser parser)
+        {
+            plainArguments = parser.plainArguments;
+            options = parser.options;
+        }
+
+        public bool ProcessArgument(string argument) // TODO: test what happens when an argument enclosed in '"' is passed. If it is already trimmed or not.
+        {            
+            if (state == ParsingState.PlainArguments)
+            {
+                return HandlePlainArgument(argument);
+            }
+
+            return DetermineArgumentType(argument) switch
+            {
+                // TODO: If the option identifier is invalid, should it be passed to optionAwaitingParameter or it should be perceived as a error?
+                ArgumentType.ShortOptionIdentifier => ProcessPossibleWaitingOption() && HandleShortOptionIdentifier(argument),
+                ArgumentType.LongOptionIdentifier => ProcessPossibleWaitingOption() && HandleLongOptionIdentifier(argument),
+                ArgumentType.PlainArgumentsDelimiter => ProcessPossibleWaitingOption() && ProcessPlainArgumentDelimiter(),
+                ArgumentType.Other when optionAwaitingParameter is not null => PassArgumentToWaitingOption(argument),
+                ArgumentType.Other => HandlePlainArgument(argument),
+                _ => false, // Cannot occur.
+            };
+        }
+
+        public bool FinalizeProcessing(out ArgumentProcessingResult result)
+        {
+            result = new();
+
+            if (!ProcessPossibleWaitingOption())
+            {
+                return false;
+            }
+
+            foreach (var option in options.MandatoryOptions)
+            {
+                if (!optionsToTakeAction.Contains(option))
+                {
+                    // todo: handle error.
+                }
+            }
+
+            foreach (var plainArgument in options.MandatoryPlainArguments)
+            {
+                if (!plainArgumentsToTakeAction.Contains(plainArgument))
+                {
+                    // todo: handle error.
+                }
+            }
+
+            result = new
+            (
+                optionsToTakeAction: optionsToTakeAction.ToArray(),
+                plainArgumentsToTakeAction: plainArgumentsToTakeAction.ToArray(),
+                excessivePlainArgumentsEntries: excessivePlainArgumentsEntries.ToArray()
+            );
+
+            optionsToTakeAction.Clear();
+            plainArgumentsToTakeAction.Clear();
+            excessivePlainArgumentsEntries.Clear();
+
+            return true;
+        }
+        private bool PassArgumentToWaitingOption(string argument)
+        {
+            if (!optionAwaitingParameter!.ProcessParameter(argument))
+            {
+                var errorMessage = $"Option could not parse the argument: \"{argument}\".";
+                Error = new(ParserErrorType.CouldNotParseTheParameter, errorMessage, optionAwaitingParameter);
+                return false;
+            }
+
+            optionsToTakeAction.Add(optionAwaitingParameter);
+            optionAwaitingParameter = null;
+            return true;
+        }
+
+        /// <summary>
+        /// Processes the option waiting for parameter that is stored in <see cref="optionAwaitingParameter"/> when there is no argument to pass.
+        /// </summary>
+        /// <returns>True if the processing was successful or there was no option waiting for an argument.</returns>
+        bool ProcessPossibleWaitingOption()
+        {
+            if (optionAwaitingParameter is null)
+            {
+                return true;
+            }
+
+            if (optionAwaitingParameter.IsParameterRequired)
+            {
+                var errorMessage = "There was no parameter for option requiring one.";
+                Error = new(ParserErrorType.MissingOptionParameter, errorMessage, optionAwaitingParameter);
+                return false;
+            }
+            else
+            {
+                optionsToTakeAction.Add(optionAwaitingParameter);
+                optionAwaitingParameter = null;
+            }
+            
+            return true;
+        }
+
+        private bool HandlePlainArgument(string argument)
+        {
+            if ((plainArguments?.Length ?? 0) <= nextPlainArgumentPosition)
+            {
+                excessivePlainArgumentsEntries.Add(argument);
+                return true;
+            }
+
+            // plainArguments want ever be null here thanks to the starting check
+            var plainArgumentToProcess = plainArguments![nextPlainArgumentPosition++];
+
+            if (!plainArgumentToProcess.ProcessParameter(argument))
+            {
+                var errorMessage = $"Option could not parse the argument: \"{argument}\".";
+                Error = new(ParserErrorType.CouldNotParseTheParameter, errorMessage, optionAwaitingParameter);
+                return false;
+            }
+
+            plainArgumentsToTakeAction.Add(plainArgumentToProcess);
+            return true;
+        }
+
+        private bool HandleShortOptionIdentifier(string identifier)
+        {
+            var findResult = options.Find(identifier[1]); // get the identifier letter.
+            if (findResult is null)
+            {
+                var errorMessage = $"Option with an identifier \"{identifier}\" could not be found.";
+                Error = new(ParserErrorType.InvalidOptionIdentifier, errorMessage);
+                return false;
+            }
+
+            if (findResult is IParametrizedOption parametrizedOption)
+            {
+                optionAwaitingParameter = parametrizedOption;
+            }
+            else
+            {
+                optionsToTakeAction.Add(findResult); // TODO: handle double occurrence.
+            }
+
+            return true;
+        }
+
+        private bool HandleLongOptionIdentifier(string identifier)
+        {
+            var splitted = identifier.Split('='); // TODO: Consider using find and substring.
+            var optionFindResult = options.Find(splitted[0].Substring(2)); // starting "--" is removed
+            if (optionFindResult is null)
+            {
+                var errorMessage = $"Option with an identifier \"{identifier}\" could not be found."; // TODO: duplicit
+                Error = new(ParserErrorType.InvalidOptionIdentifier, errorMessage);
+                return false;
+            }
+
+            if (optionFindResult is not IParametrizedOption parametrizedOption)
+            {
+                optionsToTakeAction.Add(optionFindResult);
+                return true;
+            }
+
+            // Processing parametrized option
+
+            // if there is no parameter
+            if (splitted.Length < 2)
+            {
+                if (parametrizedOption.IsParameterRequired)
+                {
+                    var errorMessage = "There was no parameter for option requiring one.";
+                    Error = new(ParserErrorType.MissingOptionParameter, errorMessage, optionFindResult);
+                    return false;
+                }
+            }
+
+            // There is a parameter, try to parse it and handle an eventual parsing error.
+            else if (!parametrizedOption.ProcessParameter(splitted[1]))
+            {
+                var errorMessage = $"Option could not parse the argument: \"{splitted[1]}\".";
+                Error = new(ParserErrorType.CouldNotParseTheParameter, errorMessage, optionAwaitingParameter);
+                return false;
+            }
+
+            // add successfully parsed option that is ready to take action that is either
+            // not requiring parameter or it has one already parsed.
+            optionsToTakeAction.Add(parametrizedOption);
+            return true;
+        }
+
+        bool ProcessPlainArgumentDelimiter()
+        {
+            state = ParsingState.PlainArguments;
+            return true;
+        }
+
+        void HandleError(string message) { }
+
+        private ArgumentType DetermineArgumentType(string argument)
+            => argument switch
+            {
+                "--" => ArgumentType.PlainArgumentsDelimiter,
+                _ when argument.Length > 2 && argument.StartsWith("--") => ArgumentType.ShortOptionIdentifier,
+                _ when argument.Length > 2 && argument.StartsWith('-') => ArgumentType.ShortOptionIdentifier,
+                _ => ArgumentType.Other
+            };
+    }
+}
+
--- ArgumentParsing/Parser.cs
+++ ArgumentParsing/Parser.cs
@@ -1,146 +1,186 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using ArgumentParsing.OptionSet;
 
-namespace ArgumentParsing
+namespace ArgumentParsing;
+
+/// <summary>
+/// Specifies what kind of error occurred during the parsing of command line.
+/// </summary>
+public enum ParserErrorType : byte
 {
     /// <summary>
-    /// Specifies what kind of error occurred during the parsing of command line.
+    /// There was no option with the specified identifier.
     /// </summary>
-    public enum ParserErrorType : byte 
+    InvalidOptionIdentifier,
+
+    /// <summary>
+    /// Option could not parse the parameter belonging to her.
+    /// </summary>
+    CouldNotParseTheParameter,
+
+    /// <summary>
+    /// There is Mandatory option missing on the command line.
+    /// </summary>
+    MissingMandatoryOption,
+
+    /// <summary>
+    /// There are not enough plain arguments to satisfy number of the mandatory plain arguments.
+    /// </summary>
+    MissingMandatoryPlainArgument,
+
+    /// <summary>
+    /// Argument for an option with a required argument was not passed.
+    /// </summary>
+    MissingOptionParameter,
+
+    /// <summary>
+    /// When other errors occur.
+    /// </summary>
+    Other
+
+}
+
+/// <summary>
+/// Object encapsulating information about an error in <see cref="Parser"/> class instance.
+/// </summary>
+public readonly struct ParserError // TODO: Consider changing ParserError to ParsingResult and returning it from Parse method.
+{
+    /// <summary>
+    /// Type of an error that has occurred.
+    /// </summary>
+    public readonly ParserErrorType type;
+
+    /// <summary>
+    /// Option related to the error.
+    /// </summary>
+    public readonly IOption? optionInError;
+
+    /// <summary>
+    /// Description of the error that has occurred.
+    /// </summary>
+    public readonly string message;
+
+    /// <summary>
+    /// Creates instance of <see cref="ParserError"/> with a specified type, option and additional info message.
+    /// </summary>
+    public ParserError(ParserErrorType type, string message, IOption? optionInError = null)
     {
-        /// <summary>
-        /// There was no option with the specified identifier.
-        /// </summary>
-        InvalidOptionIdentifier,
+        this.type = type;
+        this.optionInError = optionInError;
+        this.message = message;
+    }
+}
 
-        /// <summary>
-        /// Option could not parse the parameter belonging to her.
-        /// </summary>
-        CouldNotParseTheParameter,
+/// <summary>
+/// The Parser class enables parsing of command-line inputs.
+/// </summary>
+public sealed partial class Parser      //TODO: Michael
+{
+    partial class ArgumentProcessor { }
 
-        /// <summary>
-        /// There is Mandatory option missing on the command line.
-        /// </summary>
-        MissingMandatoryOption,
-
-        /// <summary>
-        /// There are not enough plain arguments to satisfy number of the mandatory plain arguments.
-        /// </summary>
-        MissingMandatoryPlainArgument,
-
-        /// <summary>
-        /// Argument for an option with a required argument was not passed.
-        /// </summary>
-        MissingOptionParameter,
-
-        /// <summary>
-        /// When other errors occur.
-        /// </summary>
+    enum ArgumentType : byte
+    {
+        ShortOptionIdentifier,
+        LongOptionIdentifier,
+        PlainArgumentsDelimiter,
         Other
+    }
 
+    private IPlainArgument[]? plainArguments = null;
+    private OptionSet.OptionSet options = new();
+
+    // TODO: add the null info to the documentation
+    /// <summary>
+    /// Gets remaining plain arguments, which were excessive in relation to the number of IPlainArguments passed in constructor. Null if there were no excessive plain arguments.
+    /// </summary>
+    public string[]? RemainingPlainArguments { get; private set; } = null;
+
+    /// <summary>
+    /// If an error occurs during parsing it gets a instance of <see cref="ParserError"/> that is describing the problem, otherwise null.
+    /// </summary>
+    public ParserError? Error { get; private set; } = null;
+
+    /// <summary>
+    /// Creates instance of <see cref="Parser"/> without specified types of plain parameters.
+    /// </summary>
+    public Parser()
+    {
+        plainArguments = null;
     }
 
     /// <summary>
-    /// Object encapsulating information about an error in <see cref="Parser"/> class instance.
+    /// Creates instance of <see cref="Parser"/> with specified types of plain parameters.
     /// </summary>
-    public readonly struct ParserError
+    /// <param name="plainArguments"> Array of <see cref="IParametrizedOption"/>, where mandatory plain arguments should come before the non-mandatory 
+    /// plain arguments. To create instances of these plain arguments user can use static method CreatePlainArgument of Interface IParametrized,
+    /// when he wants to define just one plain argument that should stand alone, or static method CreateMultipleParametersPlainArgument of Interface
+    /// IMultipleParameterOption when he wants to process plain arguments separated by the separator. Or he can create them manually and non-necessary
+    /// fields and properties are mentioned next to the mentioned methods.
+    /// </param>
+    public Parser(IPlainArgument[] plainArguments)
     {
-        /// <summary>
-        /// Type of an error that has occurred.
-        /// </summary>
-        public readonly ParserErrorType type;
-        
-        /// <summary>
-        /// Description of the error that has occurred.
-        /// </summary>
-        public readonly string message;        
+        this.plainArguments = plainArguments;
     }
 
     /// <summary>
-    /// The Parser class enables parsing of command-line inputs.
+    /// Adds the option to the OptionSet.
     /// </summary>
-    public sealed class Parser      //TODO: Michael
+    /// <param name="option"><see cref="IOption"> instance to be added.</param>
+    /// <returns>Returns true if there were no problems adding the option,
+    /// returns false if an error occurred, such as synonyms colliding with already added options, no short options and
+    /// no long options at the same time and other undefined behavior.
+    /// </returns>
+    public bool Add(IOption option) => options.Add(option);
+
+
+    /// <summary>
+    /// Parses a given command.
+    /// </summary>
+    /// <param name="args">Command line arguments.</param>
+    /// <returns>True when parsing was successful, otherwise false.</returns>
+    public bool ParseCommandLine(string[] args) // TODO: We will possibly need to add a method used to restore the IParametrizedOption instance, when the parsing fails.
     {
-        private IPlainArgument[]? _plainArguments;
-        private OptionSet.OptionSet _options = new();
+        // TODO: check that all mandatory options are present.
+        ArgumentProcessor argumentProcessor = new(this);
 
-        /// <summary>
-        /// Gets remaining plain arguments, which were excessive in relation to the number of IPlainArguments passed in constructor.
-        /// </summary>
-        public string[]? RemainingPlainArguments { get; private set; }
-
-        /// <summary>
-        /// Gets parser error if one occurred, otherwise null.
-        /// </summary>
-        public ParserError? Error { get; private set; }
-
-        /// <summary>
-        /// Creates instance of <see cref="Parser"/> without specified types of plain parameters.
-        /// </summary>
-        public Parser ()
+        for (int i = 0; i < args.Length; i++)
         {
-            _plainArguments = null;
+            if (!argumentProcessor.ProcessArgument(args[i]))
+            {
+                // TODO: add error handling
+                return false;
+            }
         }
 
-        /// <summary>
-        /// Creates instance of <see cref="Parser"/> with specified types of plain parameters.
-        /// </summary>
-        /// <param name="plainArguments"> Array of <see cref="IParametrizedOption"/>, where mandatory plain arguments should come before the non-mandatory 
-        /// plain arguments. To create instances of these plain arguments user can use static method CreatePlainArgument of Interface IParametrized,
-        /// when he wants to define just one plain argument that should stand alone, or static method CreateMultipleParametersPlainArgument of Interface
-        /// IMultipleParameterOption when he wants to process plain arguments separated by the separator. Or he can create them manually and non-necessary
-        /// fields and properties are mentioned next to the mentioned methods.
-        /// </param>
-        public Parser(IPlainArgument[] plainArguments)
-        {
-            _plainArguments = plainArguments;
-        }
+        // todo: finalize argument processing
+        // todo: transfer excessive plain arguments.
+        return true;
+    }
 
-        /// <summary>
-        /// Adds the option to the OptionSet.
-        /// </summary>
-        /// <param name="option"><see cref="IOption"> instance to be added.</param>
-        /// <returns>Returns true if there were no problems adding the option,
-        /// returns false if an error occurred, such as synonyms colliding with already added options, no short options and
-        /// no long options at the same time and other undefined behavior.
-        /// </returns>
-        public bool Add(IOption option) => _options.Add(option); // TODO: consider unifying the way plain arguments and options are added.
-        
+    /// <summary>
+    /// Method allows user to get the help text to be shown when client uses -h/--help on command line.
+    /// To work correctly, user must specify at each option, which hints or explanations to be showed.
+    /// </summary>
+    /// <returns>Returns string to be shown when client uses -h/--help on command line</returns>
+    public string GetHelpString()
+    {
+        throw new NotImplementedException();
+    }
 
-        /// <summary>
-        /// Parses a given command.
-        /// </summary>
-        /// <param name="args">Command line arguments.</param>
-        /// <returns>True when parsing was successful, otherwise false.</returns>
-        public bool ParseCommandLine(string [] args)
-        {
-            throw new NotImplementedException();
-        }
+    /// <summary>
+    /// Allows user set help string for "--", which is shown when -h/--help is present on command line.
+    /// </summary>
+    /// <param name="PAHelpString">Help string to be shown next to -- in help page.</param>
 
-        /// <summary>
-        /// Method allows user to get the help text to be shown when client uses -h/--help on command line.
-        /// To work correctly, user must specify at each option, which hints or explanations to be showed.
-        /// </summary>
-        /// <returns>Returns string to be shown when client uses -h/--help on command line</returns>
-        public string GetHelpString()
-        {
-            throw new NotImplementedException();
-        }
-
-        /// <summary>
-        /// Allows user set help string for "--", which is shown when -h/--help is present on command line.
-        /// </summary>
-        /// <param name="PAHelpString">Help string to be shown next to -- in help page.</param>
-
-        public void SetPlainArgumentHelpString(string PAHelpString)
-        {
-            throw new NotImplementedException();
-        }
+    public void SetPlainArgumentHelpString(string PAHelpString)
+    {
+        throw new NotImplementedException();
     }
 
 
