5ad45c7a0c55f77838634573a43e3286f5d132c8
Michal Kolcun <michalkolcun1@gmail.com>
2023-05-02 15:20:20 +0200

    add option builder tests

---
+++ MichalTests/UnitTest1.cs
@@ -0,0 +1,17 @@
+namespace MichalTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            Parser parser = new Parser();
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
